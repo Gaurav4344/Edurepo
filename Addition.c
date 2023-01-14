086f7464b7acca96a5a5f4947396f0b335b95a25
Gaurav Patil <122665090+Gaurav4344@users.noreply.github.com>
2023-01-14 14:12:22 +0530

    Add files via upload

---
+++ Addition.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int x,y,result;
+    printf("Enter one number");
+    scanf("%d",&x);
+    printf("Enter second number");
+    scanf("%d",&y);
+    result=x+y;
+    printf("Sum of two number is %d",result);
+    return 0;
+}
